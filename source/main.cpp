597fc1b428c6cd36aed00233e90db840a3fde986
Lebedev Roman <balakovo_roman@mail.ru>
2025-03-03 21:47:26 +0300

    Added thread sanitizer to ci pipeline

--- source/main.cpp
+++ source/main.cpp
@@ -5,4 +5,4 @@
 int main([[maybe_unused]] int arg, [[maybe_unused]] char **args) {
   std::cout << "Version: " << version::GetProjPatchVersion() << std::endl;
   std::cout << "Hello, world!" << std::endl;
-}
+} 
