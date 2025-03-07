e4a1a929765769bed315c8d92126ef51b992f8a1
Lebedev Roman <balakovo_roman@mail.ru>
2025-03-07 22:26:10 +0300

    Initial commit

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 
-#include "version/version.hpp"
-
 int main([[maybe_unused]] int arg, [[maybe_unused]] char **args)
 {
-    std::cout << "Version: " << version::GetProjPatchVersion() << std::endl;
     std::cout << "Hello, world!" << std::endl;
 }
