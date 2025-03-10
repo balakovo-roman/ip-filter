8a13312a434389f3cb8d4422fdf008abd795f524
Lebedev Roman <balakovo_roman@mail.ru>
2025-03-10 13:36:41 +0300

    Implement logic of application

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
-int main([[maybe_unused]] int arg, [[maybe_unused]] char **args)
+#include "ip/filter.hpp"
+#include "ip/utils.hpp"
+
+int main([[maybe_unused]] int arg, [[maybe_unused]] char** args)
 {
-    std::cout << "Hello, world!" << std::endl;
+    ip::Reader reader{std::cin};
+
+    ip::Printer printer{std::cout};
+
+    auto input_ips = reader.ReadFirstIpFromLines();
+
+    ip::Filter filter{input_ips};
+
+    ip::SortReverseLexicographical(input_ips);
+
+    const auto filtered_by_one_octet = filter.FilterByMask({1});
+
+    const auto filtered_by_two_octets = filter.FilterByMask({46, 70});
+
+    const auto filtered_by_any_octet = filter.FilterByOctetValue(46);
+
+    for (const auto& ips :
+         std::array{input_ips, filtered_by_one_octet, filtered_by_two_octets,
+                    filtered_by_any_octet})
+    {
+        printer.Print(ips);
+    }
 }
